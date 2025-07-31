6b43abfe23f3e7489a70ad42411a5d4f6e39e78b
Michael Niño <54298352+MichaelNino@users.noreply.github.com>
2025-07-31 13:14:48 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>About</h1>
+    <p>The year is {{ $year }}.</p>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+
+<h1>Hello, {{ $name }}!</h1>
+<p>Welcome to our MVC demo.</p>
+
+</body>
+</html>
